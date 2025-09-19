9c5aa6283802fa151035a84d750215a193694aca
Whatlavi <eriklascanovicario@gmail.com>
2025-09-19 11:43:23 +0200

    Actualizar home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,8 @@ export default function Home() {
   return (
     <main>
       <h1>Mi primer R6 Tracker 🎮</h1>
-      <p>Prueba inicial en Next.js desplegado en Vercel.</p>
+      <p>Peruano es el chris</p>
+      <img src="https://i.imgur.com/abcd123.png" alt="Logo" />
     </main>
   );
 }
